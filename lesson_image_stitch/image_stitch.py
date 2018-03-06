2f8aba5cb79da659618490360ba8bdd4e99f0fc9
dew.maple <dew.maple@gmail.com>
2018-03-07 00:35:45 +0800

    add split image by line

--- lesson_image_stitch/image_stitch.py
+++ lesson_image_stitch/image_stitch.py
@@ -45,26 +45,52 @@ class Stitch:
 
         return warped_affine[top_pnt[1]:bottom_pnt[1], left_pnt[0]:right_pnt[0]]
 
-    def split_image(self):
-        delta_x = 618 - 254
-        delta_y = 388 - 126
-        tan_h = delta_x / delta_y
-        y = 127 - (640 - 618) / tan_h
-        x = tan_h * (480 - 388)
-        l_pnt1 = (640, int(y))
-        l_pnt2 = (int(x), 480)
-        cv2.line(self.im_left, l_pnt1, l_pnt2, (0, 0, 255), 2)
+    @classmethod
+    def line_through(cls, point1, point2):
+        y = point2[1] - ((point2[1] - point1[1]) / (point2[0] - point1[0])) * point2[0]
+        pnt_x_0 = (0, int(y))
+        x = point2[0] - point2[1] * ((point2[0] - point1[0]) / (point2[1] - point1[1]))
+        pnt_y_0 = (int(x), 0)
+        return pnt_x_0, pnt_y_0
 
-        delta_x = 267 - 5
-        delta_y = 424 - 116
-        tan_h = delta_y / delta_x
-        y = 116 - 5 * tan_h
-        x = (480 - 424) / tan_h + 267
-        r_pnt1 = (0, int(y))
-        r_pnt2 = (int(x), 480)
-        cv2.line(self.im_right, r_pnt1, r_pnt2, (0, 0, 255), 2)
+    @classmethod
+    def against_line(cls, pnt, pnt1, pnt2):
+        """
+        ### method 1: cross product
+        """
+        # v1 = [pnt2[0] - pnt1[0], pnt2[1] - pnt1[1]]
+        # v2 = [pnt[0] - pnt1[0], pnt[1] - pnt1[1]]
+        # r = np.cross(v1, v2)
+
+        """
+        ### method 2: algebra mathematical 
+        """
+        r = (pnt1[1] - pnt2[1]) * pnt[0] + (pnt2[0] - pnt1[0]) * pnt[1] + pnt1[0] * pnt2[1] - pnt2[0] * pnt1[1]
+        return r
+
+    def split_image_1(self, image, point1=(618, 126), point2=(254, 388)):
+        pnt1, pnt2 = self.line_through(point1, point2)
+        cv2.line(image, pnt1, pnt2, (0, 255, 0), 2)
+        cv2.imshow('line', image)
+
+    def split_image(self):
+        pnt1, pnt2 = self.line_through((618, 126), (254, 388))
+        h, w = self.im_left.shape[:2]
+        for x in range(w):
+            for y in range(h):
+                if self.against_line((x, y), pnt1, pnt2) > 0:
+                    self.im_left[y, x] = (255, 255, 255)
+
+        pnt1, pnt2 = self.line_through((5, 116), (267, 424))
+
+        h, w = self.im_right.shape[:2]
+        for x in range(w):
+            for y in range(h):
+                if self.against_line((x, y), pnt1, pnt2) < 0:
+                    self.im_right[y, x] = (255, 255, 255)
 
     def stitch(self):
+        # self.split_image_1(self.im_left)
         self.split_image()
         # wraped = self.transform()
         # matcher = Matcher()
